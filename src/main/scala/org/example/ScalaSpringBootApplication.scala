ea41ee2359fb1836d1b4d441d24073a0a5732063
Vallabh <vallabhnatu96@gmail.com>
2022-06-20 21:23:58 +0530

    Initial Commit

---
+++ src/main/scala/org/example/ScalaSpringBootApplication.scala
@@ -0,0 +1,11 @@
+package org.example
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ScalaSpringBootApplication
+
+object ScalaSpringBootApplication extends App {
+  SpringApplication.run(classOf[ScalaSpringBootApplication])
+}
